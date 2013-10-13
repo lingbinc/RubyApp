3f178820cdf6e413523f92293be24959637054a9
lingbinc <lingbinc@seas.upenn.edu>
2013-10-12 23:51:30 -0400

    validation and css added

--- app/models/tasklist.rb
+++ app/models/tasklist.rb
@@ -2,4 +2,5 @@ class Tasklist < ActiveRecord::Base
   belongs_to :course
   belongs_to :user
   attr_accessible :title, :user_id, :course_id
+  validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password
+  attr_accessible :email, :name, :password, :password_confirmation
+  validates :name, presence: true, uniqueness: true
+  emailFormatValidation = /[a-zA-Z.-]+@[a-z \d .-]+[.][a-z]/i
+  validates :email, format: {with: emailFormatValidation}, uniqueness: true
+  validates :password, length: {within: 6..12}
+  validates :password, confirmation: true
 end
